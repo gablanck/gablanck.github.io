338ed05f4b236893c870948cd36cd51f38cb3e5e
gablanck <167591203+gablanck@users.noreply.github.com>
2025-03-17 21:13:40 -0400

    final edits for hobby page

--- itis3135/scripts/hobby-scripts.js
+++ itis3135/scripts/hobby-scripts.js
@@ -1,10 +1,9 @@
 function showSection(sectionId) {
-    const sections = document.querySelectorAll('main section');
-    sections.forEach(section => section.classList.remove('active'));
+    const sections = document.querySelectorAll('section');
+    sections.forEach(function (section) {
+      section.classList.remove('active');
+    });
     document.getElementById(sectionId).classList.add('active');
   }
-  window.onload = function() {
-    showSection('home');
-  };
-
+  
   document.write('<script src="https://lint.page/kit/4d0fe3.js" crossorigin="anonymous"></script>');
